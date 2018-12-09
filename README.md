c912133136605ee3d428aba983c404b84d122f8e
Zarina Mazieva <zmazieva78@gmail.com>
2018-12-08 20:37:17 -0800

    updated read me file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Word-Guess-Game
+<h1>This is Word Guessing Game</h1> 
+
+<h3>Rules are:</h3> 
+<p>Pick a letter to solve the word, you have only 10 wrong attempts. Also "start" button changes to "give up" button as soon you start the game, if you think it is hard to solve the word or you want to start new word from the begining, just press "give up" button, it will add one lost point to your score. Good Luck!</p>
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,7 +17,7 @@ function getDisplayText(letters) {
 function startGame() {
     if (document.getElementById("startButton").innerHTML == "Start Game") {
         var words = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming"];
-        var text = "";
+        // var text = "";
         var idx = Math.floor(Math.random() * words.length);
 
         guessedWord = words[idx];
